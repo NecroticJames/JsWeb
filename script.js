6fbab5433c80023775f2f3e23f22d90c9f849832
NecroticJames <gyorke.tudorfrancisc@yahoo.com>
2023-08-02 23:04:48 +0300

    Part2
    Ar trebui sa fie part 2

---
+++ script.js
@@ -0,0 +1,39 @@
+//has to be first to be used
+// lect32
+/*
+'use strict';
+
+let hasDriversLicense=false;
+const passTest=true;
+
+if(passTest)hasDriversLicense=true;
+if(hasDriversLicense) console.log(`I CAN DRIVE`);
+
+
+// const interface='Audio';
+//const private=543;
+
+//forbids us to do things
+*/
+// lect33func
+/*
+function logger(){
+    console.log('My name is Jonas');
+
+}
+
+logger();
+
+function fruitProccessor(apples, oranges)
+{
+    console.log(apples,oranges);
+    const juice=`Juice with ${apples} apples and ${oranges} oranges`;
+    return juice;
+}
+
+const appleJuice=fruitProccessor(5,0);
+console.log(appleJuice);
+
+const appleOrangeJuice=fruitProccessor(2,4);
+console.log(appleOrangeJuice);
+*/
